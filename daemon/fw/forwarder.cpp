3d65bf6c9f4351f0e3ee048553b54ee1bc4e1830
username <email>
2023-10-13 10:06:32 +0800

    无缓存概率

--- daemon/fw/forwarder.cpp
+++ daemon/fw/forwarder.cpp
@@ -369,57 +369,57 @@ Forwarder::onIncomingData(const Data& data, const FaceEndpoint& ingress)
   }
   
 //此处决定是否缓存
-  auto now=time::steady_clock::now();
-  auto Tmax=1000000000_ns;
-  auto Te=0_ns;
-  int count=0;
-  auto Te_ba=0_ns;
-  int count_ba=0;
-  double p;
-  //一个pitEntry对应一个interest，一个outrecord对应这个interest的一个上游端口
-  for (const auto& pitEntry : pitMatches) {
-    for(const auto& outRecord:pitEntry->getOutRecords()){
-        Te+= now - outRecord.getLastRenewed();
-        ++count;
-    }
-  }
-  NFD_LOG_DEBUG("Te_sum = "<<Te<<", count = "<<count);
-  Te=Te/count;
-  NFD_LOG_DEBUG("Te = "<<Te);
+  // auto now=time::steady_clock::now();
+  // auto Tmax=1000000000_ns;
+  // auto Te=0_ns;
+  // int count=0;
+  // auto Te_ba=0_ns;
+  // int count_ba=0;
+  // double p;
+  // //一个pitEntry对应一个interest，一个outrecord对应这个interest的一个上游端口
+  // for (const auto& pitEntry : pitMatches) {
+  //   for(const auto& outRecord:pitEntry->getOutRecords()){
+  //       Te+= now - outRecord.getLastRenewed();
+  //       ++count;
+  //   }
+  // }
+  // NFD_LOG_DEBUG("Te_sum = "<<Te<<", count = "<<count);
+  // Te=Te/count;
+  // NFD_LOG_DEBUG("Te = "<<Te);
 
-  for (const auto& pitEntry : m_pit) {
-    for(const auto& outRecord:pitEntry.getOutRecords()){
-        Te_ba+= now - outRecord.getLastRenewed();
-        ++count_ba;
-    }
-  }
-  NFD_LOG_DEBUG("Te_ba_sum = "<<Te_ba<<", count_ba = "<<count_ba);
-  Te_ba=Te_ba/count_ba;
-  NFD_LOG_DEBUG("Te_ba = "<<Te_ba);
+  // for (const auto& pitEntry : m_pit) {
+  //   for(const auto& outRecord:pitEntry.getOutRecords()){
+  //       Te_ba+= now - outRecord.getLastRenewed();
+  //       ++count_ba;
+  //   }
+  // }
+  // NFD_LOG_DEBUG("Te_ba_sum = "<<Te_ba<<", count_ba = "<<count_ba);
+  // Te_ba=Te_ba/count_ba;
+  // NFD_LOG_DEBUG("Te_ba = "<<Te_ba);
 
-  if(Te.count()<=std::min(Te_ba.count(),Tmax.count()/2)){
-    NFD_LOG_DEBUG("p=1");
-    p=1;
-  }
-  else if((Te.count()>std::min(Te_ba.count(),Tmax.count()/2))&&(Te.count()<=std::max(Te_ba.count(),Tmax.count()/2))){
-    p=1-double(Te.count()) / double(Tmax.count());
-    NFD_LOG_DEBUG("p=1-Te/Tmax= "<<p);
-  }
-  else if((Te.count()>std::max(Te_ba.count(),Tmax.count()/2))&&(Te.count()<Tmax.count())){
-    p=pow((1-double(Te.count()) / double(Tmax.count())),2);
-    NFD_LOG_DEBUG("p=(1-Te/Tmax)**2== "<<p);
-  }
-  else{
-    NFD_LOG_DEBUG("p=0");
-    p=0;
-  }
+  // if(Te.count()<=std::min(Te_ba.count(),Tmax.count()/2)){
+  //   NFD_LOG_DEBUG("p=1");
+  //   p=1;
+  // }
+  // else if((Te.count()>std::min(Te_ba.count(),Tmax.count()/2))&&(Te.count()<=std::max(Te_ba.count(),Tmax.count()/2))){
+  //   p=1-double(Te.count()) / double(Tmax.count());
+  //   NFD_LOG_DEBUG("p=1-Te/Tmax= "<<p);
+  // }
+  // else if((Te.count()>std::max(Te_ba.count(),Tmax.count()/2))&&(Te.count()<Tmax.count())){
+  //   p=pow((1-double(Te.count()) / double(Tmax.count())),2);
+  //   NFD_LOG_DEBUG("p=(1-Te/Tmax)**2== "<<p);
+  // }
+  // else{
+  //   NFD_LOG_DEBUG("p=0");
+  //   p=0;
+  // }
 
-  if((rand()/RAND_MAX)<p){
-    NFD_LOG_DEBUG("达到插入概率，可以插入");
-    m_cs.insert(data);
-  }
+  // if((rand()/RAND_MAX)<p){
+  //   NFD_LOG_DEBUG("达到插入概率，可以插入");
+  //   m_cs.insert(data);
+  // }
   // CS insert
-  // m_cs.insert(data);
+  m_cs.insert(data);
 
   std::set<std::pair<Face*, EndpointId>> satisfiedDownstreams;
   std::multimap<std::pair<Face*, EndpointId>, std::shared_ptr<pit::Entry>> unsatisfiedPitEntries;
