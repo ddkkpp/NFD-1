607fa1631e8e4534de89a0c88fa2ab5c6db17257
username <email>
2024-05-31 10:47:42 +0800

    .

--- daemon/fw/forwarder.cpp
+++ daemon/fw/forwarder.cpp
@@ -526,6 +526,8 @@ Forwarder::onIncomingData(const Data& data, const FaceEndpoint& ingress)
       continue;
     }
     this->onOutgoingData(*data1, *downstream.first);
+    // uint32_t del =rand()%15;
+    // getScheduler().schedule(ndn::time::milliseconds(del), [=] { onOutgoingData(*data1, *downstream.first);});
   }
 }
 
