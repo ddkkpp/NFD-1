ee8f94346a7507a08ab03b3485ce230135bb0809
dkp <dingkp520@qq.com>
2025-03-12 15:36:59 +0800

    修改consumerId处理

--- daemon/fw/forwarder.cpp
+++ daemon/fw/forwarder.cpp
@@ -167,13 +167,11 @@ Forwarder::onIncomingInterest(const Interest& interest, const FaceEndpoint& ingr
           NFD_LOG_DEBUG("seq= "<<seq<<" is malicious, drop the interest");
           return;
       }
-
-      auto consumerId = interest.getTag<lp::ConsumerIdTag>();
       auto tagRead = *(interest.getTag<ndn::lp::ConsumerIdTag>());
       // 提取高16位
-      uint32_t highBits =  tagRead >> 48 & 0xFFFFFFFF;
-      //提取中16位
-      uint32_t middleBits = tagRead >> 32 & 0x0000FFFF;
+      uint16_t highBits = (tagRead >> 48) & 0xFFFF;
+      // 提取中16位
+      uint16_t middleBits = (tagRead >> 32) & 0xFFFF;
       // 提取低32位
       uint32_t lowBits = tagRead & 0xFFFFFFFF;
       NFD_LOG_INFO("Tag value: high16=" << highBits << ", mid16=" << middleBits<< ", low32=" << lowBits);
@@ -186,7 +184,7 @@ Forwarder::onIncomingInterest(const Interest& interest, const FaceEndpoint& ingr
         isEdgeNode = true;
       }
       //中间16位设置为0，使得接下来的节点不会再判断为edge节点
-      uint64_t tagWrite = tagRead & 0xFF00FFFF;
+      uint64_t tagWrite = tagRead & 0xFFFF0000FFFFFFFF;
       interest.setTag(make_shared<ndn::lp::ConsumerIdTag>(tagWrite));
 
       //统计seq的数目到numOfInterest
@@ -353,9 +351,14 @@ Forwarder::onContentStoreHit(const Interest& interest, const FaceEndpoint& ingre
   NFD_LOG_DEBUG("onContentStoreHit interest=" << interest.getName());
 
   auto consumerId = interest.getTag<lp::ConsumerIdTag>();
-  uint32_t highBits = ((*consumerId) >> 32) & 0xFFFFFFFF; // 提取高32位
-  uint32_t lowBits = (*consumerId) & 0xFFFFFFFF;          // 提取低32位
-  NFD_LOG_DEBUG("Tag value: high32=" << highBits << ", low32=" << lowBits);
+  auto tagRead = *(interest.getTag<ndn::lp::ConsumerIdTag>());
+  // 提取高16位
+  uint16_t highBits = (tagRead >> 48) & 0xFFFF;
+  // 提取中16位
+  uint16_t middleBits = (tagRead >> 32) & 0xFFFF;
+  // 提取低32位
+  uint32_t lowBits = tagRead & 0xFFFFFFFF;
+  NFD_LOG_INFO("Tag value: high16=" << highBits << ", mid16=" << middleBits<< ", low32=" << lowBits);
   if(highBits ==0){
      NFD_LOG_DEBUG("normal user interest hit");
      numOfHitNormalUserInterest++;
@@ -794,7 +797,7 @@ void computeForwarderMetricsWDCallback(Forwarder *ptr)
   NFD_LOG_DEBUG("numOfUnpopularData= "<<ptr->numOfUnpopularData);
   NFD_LOG_DEBUG("numOfNotCacheOfUnpopularData= "<<ptr->numOfNotCacheOfUnpopularData);
   if(ptr->numOfUnpopularData!=0){
-    normalHitRatio = (double)ptr->numOfNotCacheOfUnpopularData / (double)ptr->numOfUnpopularData;
+    detectionRatio = (double)ptr->numOfNotCacheOfUnpopularData / (double)ptr->numOfUnpopularData;
     NFD_LOG_DEBUG("detectionRatio= "<<detectionRatio);
   }
 
@@ -802,11 +805,12 @@ void computeForwarderMetricsWDCallback(Forwarder *ptr)
   NFD_LOG_DEBUG("numOfPopularData= "<<ptr->numOfPopularData);
   NFD_LOG_DEBUG("numOfNotCacheOfPopularData= "<<ptr->numOfNotCacheOfPopularData);
   if(ptr->numOfPopularData!=0){
-    normalHitRatio = (double)ptr->numOfNotCacheOfPopularData / (double)ptr->numOfPopularData;
+    falseAlarmRatio = (double)ptr->numOfNotCacheOfPopularData / (double)ptr->numOfPopularData;
     NFD_LOG_DEBUG("falseAlarmRatio= "<<falseAlarmRatio);
   }
 
-  std::ofstream outFile("/home/dkp/ndnSIM(cpa-ours)/ns-3/ForwarderMetrics.txt", std::ios::app); // 或者 outFile.open("output.txt", std::ofstream::app);
+  //注意：这里的路径需要根据实际情况修改
+  std::ofstream outFile("/media/sf_ndnsim/ForwarderMetrics-sketch.txt", std::ios::app); // 或者 outFile.open("output.txt", std::ofstream::app);
   if (outFile.is_open()) {
     outFile << "nodeid="<<ptr->mynodeid<<" Hit= "<<normalHitRatio<<" DR= "<<detectionRatio<<" FR= "<<falseAlarmRatio<<std::endl;
   }
